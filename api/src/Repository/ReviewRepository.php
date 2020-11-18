6d19833d63f3c55d0b48078b119afa319bfcd132
Robert Zondervan <Robert@conduction.nl>
2020-11-18 14:28:15 +0100

    Oeps

--- api/src/Repository/ReviewRepository.php
+++ api/src/Repository/ReviewRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\Review;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method Review|null find($id, $lockMode = null, $lockVersion = null)
